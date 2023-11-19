ca5f458ad60649880458e8f8a95f1ccd33458268
Mieczysław Sobaś <m.sobas@fejm.pl>
2023-11-19 18:59:55 +0100

    Add project files.

---
+++ Blazor_M3_L10/Pages/BackgroundColor.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor_M3_L10.Pages
+{
+    public partial class BackgroundColor
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private async Task ChangeBackgroundColor()
+        {
+            await JSRuntime.InvokeVoidAsync("changeBackgroundColor");
+        }
+    }
+}
